81092644f7c458b95ce5c331a9da6664c4f9f50a
smy1308 <smy1308@gmail.com>
2024-01-16 02:25:56 +0900

    ✨ feat: faq.dto 작성
    - faq의 create-faq.dto작성.
    - faq.controller주석 추가.

--- src/faq/faq.controller.ts
+++ src/faq/faq.controller.ts
@@ -15,26 +15,31 @@ import { UpdateFaqDto } from './dto/update-faq.dto';
 export class FaqController {
   constructor(private readonly faqService: FaqService) {}
 
+  // FAQ 생성
   @Post()
   create(@Body() createFaqDto: CreateFaqDto) {
     return this.faqService.create(createFaqDto);
   }
 
+  // FAQ 조회
   @Get()
   findAll() {
     return this.faqService.findAll();
   }
 
+  // 특정 FAQ 조회
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.faqService.findOne(+id);
   }
 
+  // FAQ 수정
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateFaqDto: UpdateFaqDto) {
     return this.faqService.update(+id, updateFaqDto);
   }
 
+  // FAQ 삭제
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.faqService.remove(+id);
