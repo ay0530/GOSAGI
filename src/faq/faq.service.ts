124de85b889a21cda12836da2d3cb3f82e3b3120
smy1308 <smy1308@gmail.com>
2024-01-16 15:18:44 +0900

    ✨ feat: FAQ 서비스 작성

--- src/faq/faq.service.ts
+++ src/faq/faq.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateFaqDto } from './dto/create-faq.dto';
 import { UpdateFaqDto } from './dto/update-faq.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -11,23 +15,56 @@ export class FaqService {
     @InjectRepository(Faq)
     private readonly faqRepository: Repository<Faq>,
   ) {}
-  createFaq(createFaqDto: CreateFaqDto) {
-    return 'This action adds a new faq';
+
+  // FAQ 생성
+  async createFaq(createFaqDto: CreateFaqDto) {
+    const { title, content } = createFaqDto;
+    if (!title) {
+      throw new BadRequestException('제목을 입력해 주세요.');
+    }
+    if (!content) {
+      throw new BadRequestException('내용을 입력해 주세요.');
+    }
+    const newFaq = await this.faqRepository.save({ title, content });
+    return newFaq;
   }
 
-  getFaq() {
-    return `This action returns all faq`;
+  // FAQ 조회
+  async getFaq() {
+    const getAllFaq = await this.faqRepository.find({
+      order: { created_at: 'DESC' },
+    });
+    return getAllFaq;
   }
 
-  getOneFaq(id: number) {
-    return `This action returns a #${id} faq`;
+  // 특정 FAQ 조회
+  async getOneFaq(faqId: number) {
+    const getOneFaq = await this.faqRepository.findOne({
+      where: { id: faqId },
+    });
+    return getOneFaq;
   }
 
-  updateFaq(id: number, updateFaqDto: UpdateFaqDto) {
-    return `This action updates a #${id} faq`;
+  // FAQ 수정
+  async updateFaq(faqId: number, updateFaqDto: UpdateFaqDto) {
+    const existFaq = await this.faqRepository.findOne({
+      where: { id: faqId },
+    });
+    if (!existFaq) {
+      throw new NotFoundException('FAQ가 존재하지 않습니다.');
+    }
+    await this.faqRepository.update({ id: faqId }, updateFaqDto);
+    return existFaq;
   }
 
-  deleteFaq(id: number) {
-    return `This action removes a #${id} faq`;
+  // FAQ 삭제
+  async deleteFaq(faqId: number) {
+    const existFaq = await this.faqRepository.findOne({
+      where: { id: faqId },
+    });
+    if (!existFaq) {
+      throw new NotFoundException('FAQ가 존재하지 않습니다.');
+    }
+    await this.faqRepository.delete({ id: faqId });
   }
 }
