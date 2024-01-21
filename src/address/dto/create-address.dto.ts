c2d09f2f9842b686fa056f750f761d09d3a02e0c
smy1308 <smy1308@gmail.com>
2024-01-22 01:32:42 +0900

    ✨ feat: address의 dto작성

--- src/address/dto/create-address.dto.ts
+++ src/address/dto/create-address.dto.ts
@@ -1,1 +1,25 @@
-export class CreateAddressDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateAddressDto {
+  @IsNotEmpty({ message: '배송지명을 입력해 주세요.' })
+  address_name: string;
+
+  @IsNotEmpty({ message: '수령인을 입력해 주세요.' })
+  @IsString()
+  name: string;
+
+  @IsNumber()
+  phone: number;
+
+  @IsNotEmpty({ message: '주소를 입력해 주세요.' })
+  @IsString()
+  address: string;
+
+  @IsNotEmpty({ message: '상세주소를 입력해 주세요.' })
+  @IsString()
+  detail_address: string;
+
+  @IsNotEmpty({ message: '우편번호를 입력해 주세요.' })
+  @IsNumber()
+  post_code: number;
+}
