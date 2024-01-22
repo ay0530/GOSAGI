aae3cd991f676e429f169f72fb434c08c2703d88
smy1308 <smy1308@gmail.com>
2024-01-23 02:13:33 +0900

    🔥 del: 불필요한 import 삭제

--- src/address/address.service.ts
+++ src/address/address.service.ts
@@ -5,7 +5,6 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { Address } from './entities/address.entity';
-import { ApiQuery } from '@nestjs/swagger';
 
 @Injectable()
 export class AddressService {
